2c7f0372d21aea99c4dae8a790c81363a879f342
Carolyne1063 <carolmartoz763@gmail.com>
2025-04-27 04:08:15 +0300

    added search for patients from list functionality

--- frontend/chims/src/app/components/admin-dashboard/all-patient/all-patient.component.ts
+++ frontend/chims/src/app/components/admin-dashboard/all-patient/all-patient.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ClientService } from '../../../services/clientService';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all-patient',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './all-patient.component.html',
   styleUrl: './all-patient.component.css'
 })
 export class AllPatientComponent implements OnInit {
   patients: any[] = [];
+  searchTerm: string = ''; 
+
 
   constructor(private clientService: ClientService) {}
 
@@ -28,5 +31,11 @@ export class AllPatientComponent implements OnInit {
         console.error('Failed to fetch patients', err);
       }
     });
+  }
+
+  get filteredPatients() {  
+    return this.patients.filter(patient => 
+      (patient.fname + ' ' + patient.lname).toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
